7fa48b687078ad72ae62c92c23bdaac17e9baa94
FernandoBichuette <fernandoba2@al.insper.edu.br>
2019-09-17 21:12:28 -0300

    atu

---
+++ assets/js/projeto.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  let params = coDesExtract()
+  let value = params['key']
+  let value2 = params['pai']
+
+
+  let db = coDesConnect('https://projeto-1-co.firebaseio.com/')
+
+  db.download('/', function(data) {
+  	context=data
+    coDesReplace('.menu', context)
+
+    context = data['portfolio'][value2]['projetos'][value]
+    coDesReplace('.conteudo-proj',context)
+
+    context = data['portfolio'][value]
+    coDesReplace('.projeto-img',context)
+
+    })
+})
